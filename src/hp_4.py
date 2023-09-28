9bf4bd0de807ba9ba88de6eb91328273df0ec4fa
poojitha0205 <143359003+poojitha0205@users.noreply.github.com>
2023-09-28 13:29:33 -0500

    Update hp_4.py

--- src/hp_4.py
+++ src/hp_4.py
@@ -8,21 +8,25 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
-
+    return [datetime.strptime(date, "%Y-%m-%d").strftime("%d %b %Y") for date in old_dates]
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    pass
+    if not isinstance(start, str) or not isinstance(n, int): 
+        raise TypeError
+    else:
+        return [datetime.strptime(start, "%Y-%m-%d") + timedelta(days=i) for i in range(n)]
+
+
 
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    pass
+    return [(datetime.strptime(start_date, "%Y-%m-%d") + timedelta(days=i), value) for i, value in enumerate(values)]
 
 
 def fees_report(infile, outfile):
